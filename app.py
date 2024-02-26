7e1b950b838d4021adb18fbdcd3fa39a0b065d04
Reu3406 <157335953+Reu3406@users.noreply.github.com>
2024-02-26 11:44:28 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,130 @@
+import streamlit as st
+from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+from langchain.text_splitter import (
+    CharacterTextSplitter,
+    RecursiveCharacterTextSplitter,
+)
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chat_models import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from htmlTemplates import css, bot_template, user_template
+from langchain.prompts.chat import ChatPromptTemplate
+
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+
+
+# def get_text_chunks(text):
+# use recursivetext splitter, split on tokens
+#   text_splitter = CharacterTextSplitter(
+#      separator="\n", chunk_size=800, chunk_overlap=200, length_function=len
+# )
+# chunks = text_splitter.split_text(text)
+# return chunks
+
+
+def get_text_chunks(text):
+    text_splitter = RecursiveCharacterTextSplitter(
+        separators=["\n\n", "\n", ".", " "],
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len,
+    )
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+
+def get_vectorstore(text_chunks):
+    embeddings = OpenAIEmbeddings(
+        model="text-embedding-ada-002", openai_api_key=st.session_state.openai_key
+    )
+    # embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
+    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+    return vectorstore
+
+
+def handle_userinput(user_question):
+    response = st.session_state.conversation({"question": user_question})
+    st.session_state.chat_history = response["chat_history"]
+
+    for i, message in enumerate(st.session_state.chat_history):
+        if i % 2 == 0:
+            st.write(
+                user_template.replace("{{MSG}}", message.content),
+                unsafe_allow_html=True,
+            )
+        else:
+            st.write(
+                bot_template.replace("{{MSG}}", message.content), unsafe_allow_html=True
+            )
+
+
+def get_conversation_chain(vectorstore):
+    llm = ChatOpenAI(
+        model="gpt-4",
+        streaming=True,
+        temperature=0,
+        openai_api_key=st.session_state.openai_key,
+    )
+
+    memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=vectorstore.as_retriever(),
+        memory=memory,
+    )
+    return conversation_chain
+
+
+def main():
+
+    st.set_page_config(page_title="AI document intepreter")
+    st.write(css, unsafe_allow_html=True)
+
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = None
+    if "openai_key" not in st.session_state:
+        st.session_state.openai_key = None
+
+    st.header("Upload Your PDFs and ask me anything about them")
+    user_question = st.text_input("What would you like to know from your documents?")
+    if user_question:
+        handle_userinput(user_question)
+
+    with st.sidebar:
+        st.subheader("Your OpenAI API key:")
+        KEY = st.text_input("OpenAI API Key")
+        if KEY:
+            st.session_state.openai_key = KEY
+
+        st.subheader("Your documents")
+        pdf_docs = st.file_uploader(
+            "Upload your PDFs here and click on 'Process'", accept_multiple_files=True
+        )
+
+        if st.button("Process"):
+            with st.spinner("Processing"):
+                # get pdf text
+                raw_text = get_pdf_text(pdf_docs)
+
+                # get the text chunks
+                text_chunks = get_text_chunks(raw_text)
+                # create vector store
+                vectorstore = get_vectorstore(text_chunks)
+                # create conversation chain
+                st.session_state.conversation = get_conversation_chain(vectorstore)
+
+
+if __name__ == "__main__":
+    main()
